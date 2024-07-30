0dd113e554f7a2fbe2afe2f84b2f148556e1b23e
Sanket Singh <singhsanket143@gmail.com>
2024-07-31 01:53:06 +0530

    Added codes

--- src/main/java/org/example/quoraappclone/repositories/TagRepository.java
+++ src/main/java/org/example/quoraappclone/repositories/TagRepository.java
@@ -2,6 +2,8 @@ package org.example.quoraappclone.repositories;
 
 import org.example.quoraappclone.models.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
 }
